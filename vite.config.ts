a6a5e58307329c3e05862b8aeddd791c9a84d483
davemelk100 <davemelk@gmail.com>
2025-07-11 16:13:01 -0400

    cache flush

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,14 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './')
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
   }
 });
